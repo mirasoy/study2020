46b9bdb9d942dbddff0796c4a036a202e380b372
mirasoy <62410686+mirasoy@users.noreply.github.com>
2020-04-06 20:34:07 +0900

    Add files via upload

---
+++ ch6Q/app/GraoicEditor.java
@@ -0,0 +1,17 @@
+package app;
+
+import base.Shape;
+import devired.Circle;
+
+public class GraoicEditor {
+
+	
+	public static void main(String[] args) {
+		
+		Shape shape = new Circle();
+		shape.draw();
+		
+	}
+	
+	
+}
