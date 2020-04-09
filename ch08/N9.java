6be1cb00328607d3c779fbc94bd75a554fc7257b
mirasoy <62410686+mirasoy@users.noreply.github.com>
2020-04-09 09:33:33 +0900

    Update N9.java

--- ch08/N9.java
+++ ch08/N9.java
@@ -6,14 +6,19 @@ public class N9 {
 public static void main(String[] args) {
 	
 	
-	File newf = new File("E:\\MR\\j\\*.txt");
+	File newf = new File("E:\\MR\\j");
 	File[] all = newf.listFiles();
 	
 	int i =0;
 	for(File f:all){
-		System.out.println(f.getName()+"삭제");
-		f.delete();
-		i++;
+		
+		if(0<(f.getName().lastIndexOf(".txt"))){
+			System.out.println(f.getName()+"삭제");
+			f.delete();
+			i++;
+			
+		}
+//		
 	}
 	
 	System.out.println("총 "+i+"개의 파일 삭제");
